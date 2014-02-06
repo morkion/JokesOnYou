c94bd13160bf2d6f4aa7626a28450e8807d8f3e4
morkion <morkion@gmail.com>
2014-02-06 18:16:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+This bukkit plugin broadcasts a random joke every 5 minutes (default)  
+Time and actual jokes can be changed in the config file.  
+Licensed under Mozilla Public License 2.0.
---
+++ src/io/github/morkion/JokesOnYou.java
@@ -0,0 +1,26 @@
+package io.github.morkion;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JokesOnYou extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		saveDefaultConfig();
+		Timer timer = new Timer();
+		TimerTask sayJoke = new TimerTask() {
+			@Override
+			public void run() {
+				List<String> jokes = getConfig().getStringList("jokes");
+				int index = new Random().nextInt(jokes.size());
+				getServer().broadcastMessage(jokes.get(index));
+			}
+		};
+		timer.schedule(sayJoke, 0l, 1000*getConfig().getInt("timeBetweenJokes"));
+	}
+}
