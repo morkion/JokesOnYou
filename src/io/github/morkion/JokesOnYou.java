6c1282507b8712cced0388bc9071c3629779130d
morkion <morkion@gmail.com>
2014-02-07 16:35:26 +0200

    Added quotes around jokes, so it would be more discernable from other chat messages

--- src/io/github/morkion/JokesOnYou.java
+++ src/io/github/morkion/JokesOnYou.java
@@ -18,7 +18,7 @@ public class JokesOnYou extends JavaPlugin {
 			public void run() {
 				List<String> jokes = getConfig().getStringList("jokes");
 				int index = new Random().nextInt(jokes.size());
-				getServer().broadcastMessage(jokes.get(index));
+				getServer().broadcastMessage("\""+jokes.get(index)+"\"");
 			}
 		};
 		timer.schedule(sayJoke, 0l, 1000*getConfig().getInt("timeBetweenJokes"));
